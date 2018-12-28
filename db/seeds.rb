2b6f2e23e6a1bca26186ec4b54fcc4a0e62db1cc
bang <bangdaibin@gmail.com>
2018-12-28 20:49:00 +0700

    seed config

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,11 @@ User.create!(name:  "admin",
                password_confirmation: password)
 end
 
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   content = Faker::Lorem.sentence(5)
+#   users.each { |user| user.microposts.create!(content: content) }
+# end
 
 # Set up the fake following relationships
 users = User.all
