7f7f92e1399566eb8b91206887856b93f07cb90e
bang <bangdaibin@gmail.com>
2019-01-04 04:17:06 +0700

    sd

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
   def create
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
      
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+        redirect_back_or root_url
      
     else
       flash.now[:danger] = 'Invalid email/password combination'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # Refactored code above into:
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
 
-  before_action :admin_user, only: [:destroy, :edit]
+  before_action :admin_user, only: [:destroy]
 
    def index
  # @authors = Author.paginate( page: params[:page] )
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(root_url)
+      redirect_to(login_path)
 
     else
       render 'new'
@@ -54,25 +54,13 @@ class UsersController < ApplicationController
     redirect_to (users_url)
   end
 
-  def following
-    @title = "Following"
-    @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
-    render 'show_follow'
-  end
 
-  def followers
-    @title = "Followers"
-    @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
-  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation,:address,:phone)
+                                 :password_confirmation,:address,:phone,:admin)
   end
 
   # Before filters
